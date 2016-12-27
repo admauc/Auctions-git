8dd8d48d310b61111cf6de59b082835b14f6d397
admauc <admauc@yandex.ru>
2016-12-27 16:54:25 +0300

    Add files via upload

---
+++ Infrastructure/NinjectControllerFactory.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Ninject;
+using System.Collections.Generic;
+using System.Linq;
+using Moq;
+using Auctions.Domain.Abstract;
+using Auctions.Domain.Entities;
+using Auctions.Domain.Concrete;
+using System.Configuration;
+
+namespace Auctions.WebUI.Infrastructure
+{
+    // реализация пользовательской фабрики контроллеров,
+    // наследуясь от фабрики используемой по умолчанию
+    public class NinjectControllerFactory : DefaultControllerFactory
+    {
+        private IKernel ninjectKernel;
+        public NinjectControllerFactory()
+        {
+            // создание контейнера
+            ninjectKernel = new StandardKernel();
+            AddBindings();
+        }
+        protected override IController GetControllerInstance(RequestContext requestContext,
+        Type controllerType)
+        {
+            // получение объекта контроллера из контейнера
+            // используя его тип
+            return controllerType == null
+            ? null
+            : (IController)ninjectKernel.Get(controllerType);
+        }
+        private void AddBindings()
+        {
+            ninjectKernel.Bind<ILotRepository>().To<EFLotRepository>();
+
+        }
+    }
+}
