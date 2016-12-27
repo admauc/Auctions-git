67d4607d54c6a961e895dee19f6b54af492d0260
admauc <admauc@yandex.ru>
2016-12-27 16:49:58 +0300

    Add files via upload

---
+++ Models/LotsListViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Auctions.Domain.Entities;
+
+namespace Auctions.WebUI.Models
+{
+    public class LotsListViewModel
+    {
+        public IEnumerable<Lot> Lots { get; set; }
+        public PagingInfo PagingInfo { get; set; }
+        public string CurrentCategory { get; set; }
+        public string UserEmail { get; set; }
+        public IEnumerable<Category> Categories { get; set; }
+        public IEnumerable<Section> Sections { get; set; }
+    }
+}
